ca0a6ab224e4ad21b56e6d67f6012b3fc42aca5e
Tommi Reiman <tommi@iki.fi>
2018-08-12 15:15:31 +0300

    `lein perf repl` to run perf suite

--- project.clj
+++ project.clj
@@ -9,10 +9,14 @@
                                   [metosin/ring-http-response "0.9.0"]
                                   [org.slf4j/slf4j-nop "1.7.25"]
                                   [io.pedestal/pedestal.interceptor "0.5.4"]
-                                  [org.clojure/core.async "0.4.474"]]}}
+                                  [org.clojure/core.async "0.4.474"]]}
+             :perf {:jvm-opts ^:replace ["-server"
+                                         "-Xmx4096m"
+                                         "-Dclojure.compiler.direct-linking=true"]}}
   :plugins [[lein-eftest "0.5.2"]]
   :eftest {:multithread? false}
   :test-selectors {:default (constantly true)
                    :all (constantly true)}
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"})
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :aliases {"perf" ["with-profile" "default,dev,perf"]})
