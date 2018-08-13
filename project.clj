fc64732ce2aa5fdcbb5a9406411b570441f0afe3
Jarppe <jarppe@metosin.fi>
2018-08-13 14:11:43 +0300

    Reorganize project config.

--- project.clj
+++ project.clj
@@ -1,16 +1,17 @@
 (defproject metosin/sieppari "0.0.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.9.0" :scope "provided"]
-                 ; Optional dependency for generating graphs from interceptors
-                 [ubergraph "0.5.1" :scope "provided"]]
-  :profiles {:dev {:source-paths ["examples"]
-                   :dependencies [[eftest "0.5.2"]
+  :dependencies []
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0" :scope "provided"]
+                                  [ubergraph "0.5.1" :scope "provided"]
+                                  [eftest "0.5.2"]
                                   [metosin/testit "0.4.0-SNAPSHOT"]
                                   [criterium "0.4.4"]
                                   [metosin/ring-http-response "0.9.0"]
                                   [org.slf4j/slf4j-nop "1.7.25"]
                                   [io.pedestal/pedestal.interceptor "0.5.4"]
-                                  [org.clojure/core.async "0.4.474"]]}
+                                  [org.clojure/core.async "0.4.474"]]
+                   :source-paths ["examples"]}
              :perf {:jvm-opts ^:replace ["-server"
+                                         "-Xms4096m"
                                          "-Xmx4096m"
                                          "-Dclojure.compiler.direct-linking=true"]}}
   :plugins [[lein-eftest "0.5.2"]]
