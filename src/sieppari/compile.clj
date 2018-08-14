8b71b56f65ab28033d12f7451c39ab3ab07796e1
Jarppe <jarppe@metosin.fi>
2018-08-14 16:36:28 +0300

    Simplify, use handler f as reduce seed instead of identity.

--- src/sieppari/compile.clj
+++ src/sieppari/compile.clj
@@ -7,21 +7,21 @@
   "Accepts a chain of interceptors, returns a function that accepts
   the request and produces the response."
   [interceptor-chain]
-  (let [compile-fn (fn [next-f ^Interceptor interceptor]
-                     (fn [ctx]
-                       (let [ctx ((.enter interceptor) ctx)]
-                         (if (or (:response ctx)
-                                 (:error ctx))
-                           ctx
-                           (let [ctx (next-f ctx)]
-                             (if (:error ctx)
-                               ((.error interceptor) ctx)
-                               ((.leave interceptor) ctx)))))))
-        compiled (->> interceptor-chain
-                      (sc/into-interceptors)
-                      (reverse)
-                      (reduce compile-fn
-                              identity))]
+  (let [interceptors (->> interceptor-chain
+                          (sc/into-interceptors)
+                          (reverse))
+        compiled (reduce (fn [next-f ^Interceptor interceptor]
+                           (fn [ctx]
+                             (let [ctx ((.enter interceptor) ctx)]
+                               (if (or (:response ctx)
+                                       (:error ctx))
+                                 ctx
+                                 (let [ctx (next-f ctx)]
+                                   (if (:error ctx)
+                                     ((.error interceptor) ctx)
+                                     ((.leave interceptor) ctx)))))))
+                         (-> interceptors first :enter)
+                         (-> interceptors next))]
     (fn [request]
       (-> {:request request}
           (compiled)
