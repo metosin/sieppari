749c765e3de6107c93279d4ba0ddac71124522de
Tommi Reiman <tommi@iki.fi>
2018-08-15 08:05:54 +0300

    less code

--- src/sieppari/execute.clj
+++ src/sieppari/execute.clj
@@ -8,40 +8,30 @@
   (let [it (clojure.lang.RT/iter stack)]
     (loop [ctx ctx, stage stage]
       (if (.hasNext it)
-        (if-let [f (-> it .next stage)]
-          (let [ctx (u/try-f ctx f)
-                stage (if (:error ctx) :error :leave)]
-            (recur ctx stage))
-          (recur ctx stage))
+        (let [ctx (u/try-f ctx (-> it .next stage))]
+          (recur ctx (if (:error ctx) :error :leave)))
         ctx))))
 
 (defn- enter [ctx]
   (let [queue ^clojure.lang.PersistentQueue (:queue ctx)
         stack (:stack ctx)
-        error (:error ctx)
         interceptor (peek queue)]
     (cond
 
       (not interceptor)
       (leave ctx stack :leave)
 
-      error
+      (:error ctx)
       (leave (assoc ctx :queue nil) stack :error)
 
       :else
-      (let [queue (pop queue)
-            stack (conj stack interceptor)
-            f (or (:enter interceptor) identity)
-            ctx (-> ctx
-                    (assoc :queue queue)
-                    (assoc :stack stack)
-                    (u/try-f f))]
-        (recur ctx)))))
+      (recur (-> ctx
+                 (assoc :queue (pop queue))
+                 (assoc :stack (conj stack interceptor))
+                 (u/try-f (:enter interceptor)))))))
 
 (defn execute [interceptors request]
-  (-> (map->Context
-        {:request request
-         :queue (into PersistentQueue/EMPTY interceptors)})
+  (-> (new Context request nil nil (into PersistentQueue/EMPTY interceptors) nil)
       (enter)
       (u/throw-if-error!)
       :response))
