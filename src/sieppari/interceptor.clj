2dc2f362b98c2e2a06d539f3af27580417a11a04
Jarppe <jarppe@metosin.fi>
2018-08-15 18:26:07 +0300

    Use protocol function, no wrapper needed.

--- src/sieppari/interceptor.clj
+++ src/sieppari/interceptor.clj
@@ -4,44 +4,38 @@
 (defrecord Interceptor [name enter leave error])
 
 (defprotocol IntoInterceptor
-  (-interceptor [t] "Given a value, produce an Interceptor Record."))
+  (into-interceptor [t] "Given a value, produce an Interceptor Record."))
 
 (extend-protocol IntoInterceptor
   ; Map -> Interceptor:
   clojure.lang.IPersistentMap
-  (-interceptor [interceptor-map]
+  (into-interceptor [interceptor-map]
     (map->Interceptor interceptor-map))
 
   ; Function -> Handler interceptor:
   clojure.lang.Fn
-  (-interceptor [handler]
-    (-interceptor {:enter (fn [ctx]
-                            (let [response (handler (:request ctx))]
-                              (if (a/async? response)
-                                (a/continue response (partial assoc ctx :response))
-                                (assoc ctx :response response))))}))
+  (into-interceptor [handler]
+    (into-interceptor {:enter (fn [ctx]
+                                (let [response (handler (:request ctx))]
+                                  (if (a/async? response)
+                                    (a/continue response (partial assoc ctx :response))
+                                    (assoc ctx :response response))))}))
 
   ; Vector -> Interceptor, first element is a function to create
   ; the interceptor, rest are arguments for it:
   clojure.lang.IPersistentVector
-  (-interceptor [t]
-    (-interceptor (apply (first t) (rest t))))
+  (into-interceptor [t]
+    (into-interceptor (apply (first t) (rest t))))
 
   ; Interceptor -> Interceptor, nop:
   Interceptor
-  (-interceptor [t]
+  (into-interceptor [t]
     t)
 
   ; nil -> nil, nop:
   nil
-  (-interceptor [t]
+  (into-interceptor [t]
     nil))
-
-(defn into-interceptor
-  "Accepts an interceptor map, Interceptor, nil or
-  handler function, returns an Interceptor record."
-  [t]
-  (-interceptor t))
 
 (defn into-interceptors
   "Accepts a seq of interceptor maps, Interceptors, nils or
--- test/sieppari/interceptor_test.clj
+++ test/sieppari/interceptor_test.clj
@@ -12,25 +12,25 @@
   (fn [request]
     (+ request value)))
 
-(deftest -interceptor-test
+(deftest into-interceptor-test
   (fact "result is  record"
-    (-interceptor {}) => Interceptor)
+    (into-interceptor {}) => Interceptor)
 
   (fact "functions can be made to interceptors"
-    (-interceptor str) => {:enter fn?})
+    (into-interceptor str) => {:enter fn?})
 
   (fact "functions are treated as request handlers"
-    (-> inc -interceptor :enter (apply [{:request 41}])) => {:response 42})
+    (-> inc into-interceptor :enter (apply [{:request 41}])) => {:response 42})
 
   (fact "vectors are evaluated, interceptor factory case"
-    (-> [make-interceptor 10] -interceptor :enter (apply [{:foo 32}])) => {:foo 42})
+    (-> [make-interceptor 10] into-interceptor :enter (apply [{:foo 32}])) => {:foo 42})
 
   (fact "vectors are evaluated, handler factory case"
-    (-> [make-handler 10] -interceptor :enter (apply [{:request 32}])) => {:response 42})
+    (-> [make-handler 10] into-interceptor :enter (apply [{:request 32}])) => {:response 42})
 
-  (let [i (-interceptor {})]
+  (let [i (into-interceptor {})]
     (fact "interceptors are already interceptors"
-      (-interceptor i) => (partial identical? i)))
+      (into-interceptor i) => (partial identical? i)))
 
   (fact "nil punning"
-    (-interceptor nil) => nil))
+    (into-interceptor nil) => nil))
