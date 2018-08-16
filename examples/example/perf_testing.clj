130ee36ec0fa2aff1fbc30e098750b3b5745eb73
Jarppe <jarppe@metosin.fi>
2018-08-16 14:34:52 +0300

    More perf tests.

--- examples/example/perf_testing.clj
+++ examples/example/perf_testing.clj
@@ -2,15 +2,10 @@
   (:require [criterium.core :as criterium]
             [sieppari.core :as s]
             [sieppari.queue :as sq]
-            [sieppari.async :as sa]
+            [sieppari.async.core-async]
             [io.pedestal.interceptor :as pi]
             [io.pedestal.interceptor.chain :as pc]
             [clojure.core.async :as a]))
-
-(extend-protocol sa/AsyncContext
-  clojure.core.async.impl.protocols.Channel
-  (async? [_] true)
-  (continue [c f] (a/go (f (a/<! c)))))
 
 (defn make-capture-result-interceptor [p]
   {:leave (fn [ctx]
@@ -29,183 +24,49 @@
         async-interceptors (concat (repeat n async-interceptor)
                                    [identity])
 
+        p-sync-chain (doall (map pi/interceptor sync-interceptors))
+
         s-sync-chain (sq/into-queue sync-interceptors)
         s-async-chain (sq/into-queue async-interceptors)]
     (println "\n\nn =" n)
 
-    (println "\n\npedestal sync:")
-    (criterium/quick-bench
-      (let [p (promise)
-            interceptors (concat [(make-capture-result-interceptor p)]
-                                 (repeat 10 sync-interceptor)
-                                 [identity])]
-        (->> (map pi/interceptor interceptors)
-             (pc/enqueue {})
-             (pc/execute))
-        @p))
+    (do (println "\n\npedestal sync:")
+        (criterium/quick-bench
+          (->> p-sync-chain
+               (pc/enqueue {})
+               (pc/execute))))
 
-    (println "\n\npedestal async:")
-    (criterium/quick-bench
-      (let [p (promise)
-            interceptors (concat [(make-capture-result-interceptor p)]
-                                 (repeat 10 async-interceptor)
-                                 [identity])]
-        (->> (map pi/interceptor interceptors)
-             (pc/enqueue {})
-             (pc/execute))
-        @p))
+    (do (println "\n\npedestal async:")
+        (criterium/quick-bench
+          (let [p (promise)
+                interceptors (concat [(make-capture-result-interceptor p)]
+                                     (repeat 10 async-interceptor)
+                                     [identity])]
+            (->> (map pi/interceptor interceptors)
+                 (pc/enqueue {})
+                 (pc/execute))
+            @p)))
 
-    (println "\n\nsieppari sync:")
+    (println "\n\nsieppari sync chain, sync execute:")
     (criterium/quick-bench
       (s/execute s-sync-chain {}))
 
-    (println "\n\nsieppari async:")
+    (println "\n\nsieppari sync chain, async execute:")
+    (criterium/quick-bench
+      (let [p (promise)]
+        (s/execute s-sync-chain {} (partial deliver p) identity)
+        @p))
+
+    (println "\n\nsieppari async chain, sync execute:")
     (criterium/quick-bench
       (s/execute s-async-chain {}))
+
+    (println "\n\nsieppari async chain, async execute:")
+    (criterium/quick-bench
+      (let [p (promise)]
+        (s/execute s-async-chain {} (partial deliver p) identity)
+        @p))
     ))
 
-(defn run-async-perf-test [n]
-  (let [sync-interceptor {:enter identity}
-        sync-interceptors (concat (repeat n sync-interceptor)
-                                  [identity])
-        sync-chain (sq/into-queue sync-interceptors)
-
-        core-async-interceptor {:enter (fn [ctx] (a/go ctx))}
-        core-async-interceptors (concat (repeat n core-async-interceptor)
-                                        [identity])
-        core-async-chain (sq/into-queue core-async-interceptors)]
-
-    (println "n =" n)
-
-    (println "\n\nsync:")
-    (criterium/quick-bench
-      (s/execute sync-chain {}))
-
-    (println "\n\nasync:")
-    (criterium/quick-bench
-      (s/execute core-async-chain {}))
-    ))
-
-(defn -main [& [test-name count]]
+(defn -main [& _]
   (run-simple-perf-test 100))
-
-(comment
-
-  (run-async-perf-test 10)
-
-  (def d 20)
-
-  (let [p (promise)
-        s (System/nanoTime)]
-    (s/execute (concat (repeat d {:enter (fn [ctx] (update ctx :request inc))})
-                       [identity])
-               0
-               (partial deliver p))
-    (let [r (deref p 100 :timeout)
-          d (- (System/nanoTime) s)]
-      [r (format "%.3f µs" (/ d 1000.0))]))
-
-  (let [p (promise)
-        s (System/nanoTime)]
-    (s/execute (concat (repeat d {:enter (fn [ctx] (a/go (update ctx :request inc)))})
-                       [identity])
-               0
-               (partial deliver p))
-    (let [r (deref p 100 :timeout)
-          d (- (System/nanoTime) s)]
-      [r (format "%.3f µs" (/ d 1000.0))]))
-
-  (let [p (promise)
-        s (System/nanoTime)
-        r (s/execute (concat (repeat d {:enter (fn [ctx] (a/go (update ctx :request inc)))})
-                             [identity])
-                     0)
-        d (- (System/nanoTime) s)]
-    [r (format "%.3f µs" (/ d 1000.0))])
-
-  )
-
-;;
-;; How?
-;;
-
-(comment
-
-  ; Does P call :error in interceptor if same interceptor :enter fails:
-  ; Yes
-
-  (->> [{:name :a
-         :error (fn [ctx e] (println ":a error") ctx)}
-        {:name :b
-         :enter (fn [ctx] (throw (ex-info "oh no" {})))
-         :error (fn [ctx e] (println ":b error") (throw e))}]
-       (map pi/interceptor)
-       (pc/execute {})
-       :response)
-
-  ; Prints:
-  ;  :b error
-  ;  :a error
-  ;=> nil
-
-  ; Does P call :leave in interceptor if same interceptor :enter terminates:
-  ; Yes
-
-  (->> [{:name :a
-         :leave (fn [ctx] (println ":a leave") ctx)}
-        {:name :b
-         :enter (fn [ctx] (pc/terminate ctx))
-         :leave (fn [ctx] (println ":b leave") ctx)}]
-       (map pi/interceptor)
-       (pc/execute {})
-       :response)
-
-  ; Prints:
-  ;  :b leave
-  ;  :a leave
-  ;=> nil
-
-  )
-
-(comment
-
-
-
-  (let [p (promise)
-        interceptor {:enter (fn [ctx] (a/go (update ctx :request inc)))}
-        interceptors (concat [(make-capture-result-interceptor p)]
-                             (repeat 10 interceptor)
-                             [identity])]
-    (->> (map pi/interceptor interceptors)
-         (pc/enqueue {:request 0})
-         (pc/execute))
-    @p)
-
-  (let [
-        chain (->> interceptors
-                   (map pi/interceptor)
-                   (doall))]
-    (-> {:request 0}
-        (pc/enqueue chain)
-        (pc/execute)))
-  ;=> {:request 0, :response 0}
-
-  (let [p (promise)
-        capture-response {:leave (fn [ctx]
-                                   (deliver p (:response ctx))
-                                   ctx)}
-        interceptor {:enter (fn [ctx]
-                              (a/go ctx))}
-        interceptors (concat [capture-response]
-                             (repeat 3 interceptor)
-                             [identity])
-        chain (->> interceptors
-                   (map pi/interceptor))]
-    (-> {:request 0}
-        (pc/enqueue chain)
-        (pc/execute))
-    @p)
-  ;=> 0
-
-  )
-
