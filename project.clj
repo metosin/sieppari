6afba84df201e63818d1d4bd887b7d1ab7c8ffda
Jarppe <jarppe@metosin.fi>
2018-08-18 12:47:21 +0300

    .

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "https://github.com/metosin/sieppari"
   :license {:name "Eclipse Public License", :url "https://www.eclipse.org/legal/epl-2.0/"}
   :deploy-repositories [["releases" :clojars]]
+  :lein-release {:deploy-via :clojars}
 
   :dependencies []
 
