1d45c976943fdb67ea9e25762eaabc5ea2ddf82e
Pauli Jaakkola <pauli.jaakkola@metosin.fi>
2019-09-03 16:36:36 +0300

    Update REPL testing facility.

--- HACKING.md
+++ HACKING.md
@@ -6,3 +6,9 @@
 lein kaocha-clj unit # JVM
 lein kaocha-cljs unit-cljs # cljs on node
 ```
+
+## REPL
+
+```clojure
+(kaocha/run :unit) # `user` ns has alias `kaocha` -> `kaocha.repl`
+```
--- dev/user.clj
+++ dev/user.clj
@@ -1,30 +1,7 @@
 (ns user
   (:require [clojure.tools.namespace.repl :as repl]
-            #_[eftest.runner :as eftest]
-            #_[eftest.report.pretty :as pretty]))
+            [kaocha.repl :as kaocha]))
 
 (def reset repl/refresh)
 (def start (constantly :ok))
 (def stop (constantly :ok))
-
-
-;;
-;; Running tests:
-;;
-#_
-(defn run-unit-tests []
-  (eftest/run-tests
-    (->> ["test" "core/test"]
-         (mapcat eftest.runner/find-tests))
-    {:multithread?   true
-     :test-warn-time 100
-     :report         pretty/report}))
-#_
-(defn run-all-tests []
-  (eftest/run-tests
-    (->> ["test" "core/test" "test-i" "core/test-i"]
-         (mapcat eftest.runner/find-tests))
-    {:multithread?   true
-     :test-warn-time 1000
-     :report         pretty/report}))
-
