30e6c17e1e51c16ec6dce512ca911186e991bec0
Pauli Jaakkola <pauli.jaakkola@metosin.fi>
2019-09-04 16:24:55 +0300

    Deduplicate dev dependencies.

--- project.clj
+++ project.clj
@@ -7,36 +7,26 @@
 
   :dependencies []
 
-  :profiles {:test-common {:source-paths ["test/cljc"]
-                           :dependencies [[org.clojure/clojure "1.10.0" :scope "provided"]
-                                          ;; Add-ons:
-                                          [org.clojure/core.async "0.4.500"]
-                                          [manifold "0.1.8"]
-                                          [funcool/promesa "3.0.0"]
-                                          ;; Testing:
-                                          [metosin/testit "0.4.0"]
-                                          [lambdaisland/kaocha "0.0-529"]]}
-             :test-clj    {:source-paths ["test/clj"]}
-             :test-cljs   {:source-paths ["test/cljs"]
-                           :dependencies [[org.clojure/clojurescript "1.10.520"]
-                                          [lambdaisland/kaocha-cljs "0.0-40"]]}
-             :dev         {:source-paths ["dev" "test/cljc" "test/clj"]
-                           :dependencies [[org.clojure/clojure "1.10.0" :scope "provided"]
+  :profiles {:dev-deps    {:dependencies [[org.clojure/clojure "1.10.0" :scope "provided"]
                                           [org.clojure/clojurescript "1.10.520"]
                                           ;; Add-ons:
                                           [org.clojure/core.async "0.4.500"]
                                           [manifold "0.1.8"]
                                           [funcool/promesa "3.0.0"]
-                                          ;; Dev:
-                                          [org.clojure/tools.namespace "0.2.11"]
                                           ;; Testing:
                                           [metosin/testit "0.4.0"]
                                           [lambdaisland/kaocha "0.0-529"]
                                           [lambdaisland/kaocha-cljs "0.0-40"]
+                                          ;; Dev:
+                                          [org.clojure/tools.namespace "0.2.11"]
                                           ;; Perf testing:
                                           [criterium "0.4.5"]
                                           [io.pedestal/pedestal.interceptor "0.5.7"]
                                           [org.slf4j/slf4j-nop "1.7.28"]]}
+             :test-common [:dev-deps {:source-paths ["test/cljc"]}]
+             :test-clj    {:source-paths ["test/clj"]}
+             :test-cljs   {:source-paths ["test/cljs"]}
+             :dev         [:dev-deps {:source-paths ["dev" "test/cljc" "test/clj"]}]
              :examples    {:source-paths ["examples"]}
              :perf        {:jvm-opts ^:replace ["-server" "-Xms4096m" "-Xmx4096m" "-Dclojure.compiler.direct-linking=true"]}}
 
